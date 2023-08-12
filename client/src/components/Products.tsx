5ca3768b04777987d58608c504dd96004ab58576
john-George510 <meetjohn510@gmail.com>
2023-08-12 15:05:42 +0530

    type declared for product

--- client/src/components/Products.tsx
+++ client/src/components/Products.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 20px;
@@ -8,8 +10,31 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 `;
-
-function Products() {
+interface productProps {
+  category: string;
+  filters: any;
+  sort: any;
+}
+function Products({ category, filters, sort }: productProps) {
+  const [products, setProducts] = useState([]);
+  // const [filteredProducts, setfilteredProducts] = useState([]);
+  console.log(filters, sort);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          category
+            ? `https://shopnest-87me.onrender.com/api/products?category=${category}`
+            : `https://shopnest-87me.onrender.com/api/products`
+        );
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, [category]);
+  console.log(products);
   return (
     <Container>
       {popularProducts.map((item) => (
